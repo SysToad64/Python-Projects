ca20a657abb5242496a416d89ddf88e7e618b2fd
SysToad64 <116862894+SysToad64@users.noreply.github.com>
2022-10-30 23:04:12 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import webbrowser as webb
+while True:
+    webb.open_new_tab("http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcTp3fH8dtCLVjVS2yoCDOac16RvN7T0F0VvSR3XdRih4tmU4c_mKrlMKr8LeubqwyDS9Tfo6IRL7BjEyX8")
+    webb.open_new_tab("https://cdn.britannica.com/85/4685-004-093BB6DE/American-toad.jpg")
+    webb.open_new_tab("https://upload.wikimedia.org/wikipedia/commons/0/08/Bufo_bufo_sitting-Iric2006.jpg")
+    webb.open_new_tab("http://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Amphibians/amphibian_toads_600x300.ashx")
+    webb.open_new_tab("https://image.shutterstock.com/image-photo/cane-toad-rhinella-marina-big-260nw-1138251083.jpg")
